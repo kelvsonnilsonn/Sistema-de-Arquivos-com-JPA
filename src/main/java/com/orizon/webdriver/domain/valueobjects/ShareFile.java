5f1d57f94937deba34bd431b5c2517a5790a6f73
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-05-16 14:26:50 -0300

    Adicionado arquivo que representa o compartilhamento.

---
+++ src/main/java/com/orizon/webdriver/domain/valueobjects/ShareFile.java
@@ -0,0 +1,12 @@
+package com.orizon.webdriver.domain.valueobjects;
+
+import java.time.Instant;
+
+public class ShareFile {
+
+    private long id;
+    private int owner_id;
+    private int receiver_id;
+    private Instant date;
+
+}
